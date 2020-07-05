40ffa29cb60dfa6fb3d015cfaf042b70b0e9008d
farisdx66 <farisdx66@gmail.com>
2020-07-05 18:13:36 +0700

    user

--- app/config/dbConfig.js
+++ app/config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
+    USER: "rose",
+    PASSWORD: "",
     DB: "cararena_be",
     dialect: "mysql",
     pool: {
